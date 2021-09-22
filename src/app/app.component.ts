7f255465368c4cc3978d5539cd6ccb2f63aa79a7
Michael Roberts <Michael.Roberts@wakegov.com>
2021-09-22 13:13:44 -0400

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'InterviewAssignment-NG';
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {    
+    this.httpClient.get<any>('https://localhost:5001/employees').subscribe(data => {
+      console.log(data);
+    })   
+  }
 }
